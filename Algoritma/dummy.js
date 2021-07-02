4a5a8d28bd5af28a50640a9dcacc12c37cbbd711
NaNnin08 <ocotsunandar@gmail.com>
2021-07-02 20:50:18 +0700

    aad

--- Algoritma/dummy.js
+++ Algoritma/dummy.js
@@ -1,13 +1,7 @@
-const BigInt = require("big-integer");
+const orderBy = (id) => {
+  const order = id > 0 ? id > 5 && "LEBIH DARI LIMA" : "NOL";
 
-let arr = [122112121211212231231231231231231231231231231231313122121,1,2,34,5];
+  console.log(order);
+};
 
-arr = arr.sort((a,b) => {
-    if (a.length == b.length) { a - b }
-        else{
-            a.length - b.length
-        }
-});
-// arr = arr.sort((a, b) => toString(a).length - toString(b).length);
-console.log(arr);
-
+orderBy(4);
